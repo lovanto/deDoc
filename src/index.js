288c0c196f9982ac78bf8987259cf86d3639f980
Lovanto R <rifkylovanto@gmail.com>
2020-09-14 09:40:03 +0700

    Init File -> Hello World

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <React.StrictMode>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
