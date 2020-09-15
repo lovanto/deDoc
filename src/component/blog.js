d2df391127f4dbb4ffc69ce231f2cc4806c384cc
Lovanto R <rifkylovanto@gmail.com>
2020-09-15 22:00:41 +0700

    Reuse card blog implemented

--- src/component/blog.js
+++ src/component/blog.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Footer from './footer.js';
+import CardBlog from './cardBlog.js';
 
 const Blog = () => {
     return (
@@ -14,39 +15,9 @@ const Blog = () => {
                     </div>
                     <div className="row">
                         <div className="d-flex justify-content-center mb-5">
-                            <div className="card col-md-3 set-padding-0 m-3">
-                                <div className="card-img-top img-bg" />
-                                <div className="card-body">
-                                    <p className="card-text font-weight-bold category">Category | Date</p>
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <div className="d-flex justify-content-center">
-                                        <a href="/" className="btn btn-info">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card col-md-3 set-padding-0 m-3">
-                                <div className="card-img-top img-bg" />
-                                <div className="card-body">
-                                    <p className="card-text font-weight-bold category">Category | Date</p>
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <div className="d-flex justify-content-center">
-                                        <a href="/" className="btn btn-info">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card col-md-3 set-padding-0 m-3">
-                                <div className="card-img-top img-bg" />
-                                <div className="card-body">
-                                    <p className="card-text font-weight-bold category">Category | Date</p>
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <div className="d-flex justify-content-center">
-                                        <a href="/" className="btn btn-info">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
+                            <CardBlog/>
+                            <CardBlog/>
+                            <CardBlog/>
                         </div>
                         <div className="col-md-12 d-flex justify-content-center mb-5">
                             <a href="/" className="btn btn-primary btn-view">View More</a>
---
+++ src/component/cardBlog.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CardBlog = ({ category = "Category", date = "19-09-2020", title = "Title Post", description = "Ini adalah bagian dari deskripsi singkat tentang postingan ini.", img, link = "/" }) => {
+    return (
+        <div className="card col-md-3 set-padding-0 m-3">
+            <div className="card-img-top img-bg" />
+            <div className="card-body">
+                <p className="card-text font-weight-bold category">{category} | {date}</p>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <div className="d-flex justify-content-center">
+                    <a href={`/` + link} className="btn btn-info">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CardBlog;
