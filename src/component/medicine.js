ff9b93725298e28acc99ae4a645db70ff7299331
Lovanto R <rifkylovanto@gmail.com>
2020-09-17 14:52:01 +0700

    sketch of medic page

--- src/component/medicine.js
+++ src/component/medicine.js
@@ -5,7 +5,88 @@ const Medicine = () => {
     return (
         <div>
             <article>
-                <div><center><h1>INI PAGE MEDICINE</h1></center></div>
+                <div className="col-md-12 text-center m-5 p-5 banner-medic">
+                    <div className="header">Pilih Obat Sesuai Kategori</div>
+                    <div className="sub-header">Pilih Obat Sesuai Kategori</div>
+                </div>
+                <div className="d-flex justify-content-center mb-5">
+                    <div className="col-md-8">
+                        <div className="card card-rounded">
+                            <div className="card-body">
+                                <div className="row mr-4 ml-4 mt-3 mb-3 d-flex justify-content-center">
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                    <div>
+                                        <div className="circle-btn mr-4 ml-4"></div>
+                                        <div className="text-center mt-3 mb-5">adwad</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-center mb-5 pb-5">
+                    <div className="col-md-8">
+                        <div className="card card-rounded">
+                            <div className="card-body m-5 p-5">
+                                <div className="text-left subheader-detail min-l-1">Penjelasan</div><br/>
+                                <div className="row d-flex justify-content-center col-md-11">
+                                    1.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                    laboris nisi ut aliquip ex ea commodo consequat. <br /><br />
+                                    2.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                    laboris nisi ut aliquip ex ea commodo consequat. <br /><br />
+                                    3.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                    laboris nisi ut aliquip ex ea commodo consequat. <br /><br />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </article>
             <Footer />
         </div>
